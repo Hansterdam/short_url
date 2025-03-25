19bbe698abf534aeb9d62a62454376eee3d48715
Han Kortekaas <han@exivity.com>
2025-03-25 16:43:54 +0100

    Make second test pass with dummy code

--- lib/short_url_web/controllers/short_url_controller.ex
+++ lib/short_url_web/controllers/short_url_controller.ex
@@ -1,7 +1,7 @@
 defmodule ShortUrlWeb.ShortUrlController do
   use ShortUrlWeb, :controller
 
-  def shorten(conn, _params) do
-    conn
+  def shorten(conn, params) do
+    json(conn, %{"url" => params["url"], "short_url" => "http://localhost:4000/abc123"})
   end
 end
--- test/short_url_web/controllers/short_url_controller_test.exs
+++ test/short_url_web/controllers/short_url_controller_test.exs
@@ -5,4 +5,13 @@ defmodule ShortUrlWeb.ShortUrlControllerTest do
     conn = get(conn, ~p"/shorten_url")
     assert json_response(conn, 404) == %{"errors" => %{"detail" => "Not Found"}}
   end
+
+  test "POST /shorten_url returns a shortened url", %{conn: conn} do
+    conn = post(conn, ~p"/shorten_url", %{"url" => "http://very-long-url.com"})
+
+    assert json_response(conn, 200) == %{
+             "url" => "http://very-long-url.com",
+             "short_url" => "http://localhost:4000/abc123"
+           }
+  end
 end
