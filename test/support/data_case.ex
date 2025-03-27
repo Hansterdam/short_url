9efb91a8b668d9aa21a92002802a1858e3fab25d
Han Kortekaas <han@exivity.com>
2025-03-27 15:39:29 +0100

    Remove unneeded change

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -35,8 +35,8 @@ defmodule ShortUrl.DataCase do
   @doc """
   Sets up the sandbox based on the test tags.
   """
-  def setup_sandbox(_tags) do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(ShortUrl.Repo, shared: false)
+  def setup_sandbox(tags) do
+    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(ShortUrl.Repo, shared: not tags[:async])
     on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
   end
 
